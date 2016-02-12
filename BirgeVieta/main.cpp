2b4ba34093a5d268fb67603eca7a5d6785327b2b
Melissa Trevino <melissatrevino@melissas-macbook-pro-2.local>
2016-02-11 20:57:37 -0600

    Initial Commit

---
+++ BirgeVieta/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BirgeVieta
+//
+//  Created by Melissa Trevino on 2/11/16.
+//  Copyright © 2016 Melissa Trevino. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
